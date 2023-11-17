feb1cf09d0798ba98a54b03036ff5b0db7015f05
mohsen <mohsenseyedi1382@gmail.com>
2023-11-17 19:14:58 +0330

    get show all users to doshboard

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,8 +1,61 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
+import ErrorBox from '../ErrorBox/ErrorBox'
 
 export default function Users() {
 
+  const [users , setUsers] = useState([])
+
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/users/")
+    .then(res => res.json())
+    .then(data => {
+      setUsers(data)
+      console.log(data);
+    })
+  },[])
   return (
-    <div></div>
+    <div>
+        <table className="products-table bg-white-color w-full px-12 py-10 rounded-2xl">
+          <thead>
+            <tr className="text-center flex justify-between pt-4 pr-[50px] pl-[240px] border-b">
+              <th className="p-5">اسم</th>
+              <th className="p-5">آدرس</th>
+              <th className="p-5">شماره تماس</th>
+              <th className="p-5">امتیاز</th>
+            </tr>
+          </thead>
+          <tbody>
+           
+          {
+            !users.length ? (
+              <ErrorBox msg={"هیچ کاربری یافت نشد"} />
+            ): 
+            users.map(user => (
+              <tr
+              key={user.id}
+              className="text-center flex justify-between pr-[15px] pl-[20px] border-b"
+            >
+              <td className="p-5">{user.firsname} {user.lastname}</td>
+              <td className="p-5">{user.address}</td>
+              <td className="p-5">{user.phone}</td>
+              <td className="p-5">{user.score}</td>
+              <td className="p-5">
+                <button className="btn mr-1">جزییات</button>
+                <button
+                  
+                  className="btn mr-1"
+                >
+                  حذف
+                </button>
+                <button className="btn mr-1">ویرایش</button>
+              </td>
+            </tr>
+            ))
+          }
+         
+          </tbody>
+        </table>
+    </div>
   )
 }
